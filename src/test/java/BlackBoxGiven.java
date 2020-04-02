8db728749fb4475d69a120bc4f80ca5c604b8cbd
Kevin Ayers <kevin.ayers123@me.com>
2020-04-01 17:48:50 -0700

    Black Box tests

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -107,12 +107,70 @@ public class BlackBoxGiven {
     
     Cart mixedCartDiscType1;
     double mixedCartDiscType1Expected;
+    double amountSavedCartDiscType1Expected;
     
     Cart mixedCartDiscType2;
     double mixedCartDiscType2Expected;
+    double amountSavedCartDiscType2Expected;
     
     Cart mixedCartDiscTwoTypes;
     double mixedCartTwoTypesExpected;
+    double amountSavedCartDiscTwoTypesExpected; 
+    
+    
+    ///////////////////////////////////
+    
+    Cart cartAlcohol1UnderAge;
+    Cart cartAlcohol5UnderAge;
+    Cart cartAlcohol10UnderAge;
+    Cart cartAlcohol100UnderAge;
+    
+    Cart cartAlcohol121;
+    Cart cartAlcohol521;
+    Cart cartAlcohol1021;
+    Cart cartAlcohol10021;
+    double cartAlcohol1Expected21;
+    double cartAlcohol5Expected21;
+    double cartAlcohol10Expected21;
+    double cartAlcohol100Expected21;
+    
+    Cart cartEmpty;
+    double cartEmptyExpected; 
+    
+    Cart cartEmpty21;
+    double cartEmpty21Expected;
+    
+    Cart cartEmptyUnderAge;
+    double cartEmptyUnderAgeExpected;
+    
+    //edger cases
+    Cart edgeCaseAge20;
+    double edgeCaseAge20Expected;
+    
+    Cart edgeCaseAge22;
+    double edgeCaseAge22Expected; 
+    
+    Cart edgeCaseProduce1;
+    double edgeCaseProduce1Expected;
+    
+    Cart edgeCaseProduce2;
+    double edgeCaseProduce2Expected;
+    
+    Cart edgeCaseAlcFroz1;
+    double edgeCaseAlcFroz1Expected;
+    
+    Cart edgeCaseAlcFroz2;
+    double edgeCaseAlcFroz2Expected;
+    
+    Cart edgeCaseNoAlc1;
+    double edgeCaseNoAlc1Expected;
+    
+    Cart edgeCaseNoAlc2;
+    double edgeCaseNoAlc2Expected;
+    
+    Cart edgeCaseNoAlc3;
+    double edgeCaseNoAlc3Expected; 
+
     
     
    
@@ -140,65 +198,65 @@ public class BlackBoxGiven {
 
         cart1Expected = 70.2;
         
-        cartDairy1 = createCart(30);
+        cartDairy1 = createCart(21);
         for (int i = 0; i< 1; i++) {
         	cartDairy1.addItem(new Dairy());
         }
         cartDairy1Expected = 3.24;
         
-        cartDairy5 = createCart(30);
+        cartDairy5 = createCart(21);
         for (int i = 0; i< 5; i++) {
         	cartDairy5.addItem(new Dairy());
         }
         cartDairy5Expected = 16.2;
         
-        cartDairy10 = createCart(30);
+        cartDairy10 = createCart(21);
         for (int i = 0; i< 10; i++) {
         	cartDairy10.addItem(new Dairy());
         }
         cartDairy10Expected = 32.4;
         
-        cartDairy100 = createCart(30);
+        cartDairy100 = createCart(21);
         for (int i = 0; i< 100; i++) {
         	cartDairy100.addItem(new Dairy());
         }
         cartDairy100Expected = 324;
         
-        cartMeat1 = createCart(30);
+        cartMeat1 = createCart(21);
         for (int i =0; i < 1; i++) {
         	cartMeat1.addItem(new Meat());
         }
         cartMeat1Expected = 10.80; 
         
-        cartMeat5 = createCart(30);
+        cartMeat5 = createCart(21);
         for (int i =0; i < 5; i++) {
         	cartMeat5.addItem(new Meat());
         }
         
         cartMeat5Expected = 54.0; 
        
-        cartMeat10 = createCart(30);
+        cartMeat10 = createCart(21);
         for (int i =0; i < 10; i++) {
         	cartMeat10.addItem(new Meat());
         }
         
         cartMeat10Expected = 108; 
         
-        cartMeat100 = createCart(30);
+        cartMeat100 = createCart(21);
         for (int i =0; i < 100; i++) {
         	cartMeat100.addItem(new Meat());
         }
         
         cartMeat100Expected = 1080.0; 
         
-        cartProduce1 = createCart(30);
+        cartProduce1 = createCart(21);
         for (int i =0; i < 1; i++) {
         	cartProduce1.addItem(new Produce());
         }
         
         cartProduce1Expected = 2.16;
         
-        cartProduce5 = createCart(30);
+        cartProduce5 = createCart(21);
         for (int i =0; i < 5; i++) {
         	cartProduce5.addItem(new Produce());
         }
@@ -206,21 +264,21 @@ public class BlackBoxGiven {
         cartProduce5Expected = 9.72;
         
 
-        cartProduce10 = createCart(30);
+        cartProduce10 = createCart(21);
         for (int i =0; i < 10; i++) {
         	cartProduce10.addItem(new Produce());
         }
         
         cartProduce10Expected = 18.36;
         
-        cartProduce100 = createCart(30);
+        cartProduce100 = createCart(21);
         for (int i =0; i < 100; i++) {
         	cartProduce100.addItem(new Produce());
         }
         
         cartProduce100Expected = 180.36;
         
-        cartProduce3= createCart(30);
+        cartProduce3= createCart(21);
         for (int i = 0; i < 3; i++) {
         	cartProduce3.addItem(new Produce());
         }
@@ -228,56 +286,56 @@ public class BlackBoxGiven {
         cartProduce3Expected = 5.40;
         
         
-        cartAlcohol1 = createCart(30);
+        cartAlcohol1 = createCart(21);
         for (int i = 0; i < 1; i++) {
         	cartAlcohol1.addItem(new Alcohol());
         }
         
         cartAlcohol1Expected = 8.64;
         
-        cartAlcohol5 = createCart(30);
+        cartAlcohol5 = createCart(21);
         for (int i = 0; i < 5; i++) {
         	cartAlcohol5.addItem(new Alcohol());
         }
         
         cartAlcohol5Expected = 43.2;
         
-        cartAlcohol10 = createCart(30);
+        cartAlcohol10 = createCart(21);
         for (int i = 0; i < 10; i++) {
         	cartAlcohol10.addItem(new Alcohol());
         }
         
         cartAlcohol10Expected = 86.4;
         
-        cartAlcohol100 = createCart(30);
+        cartAlcohol100 = createCart(21);
         for (int i = 0; i < 100; i++) {
         	cartAlcohol100.addItem(new Alcohol());
         }
         
         cartAlcohol100Expected = 864;
         
-        cartFrozenFood1 = createCart(30);
+        cartFrozenFood1 = createCart(21);
         for (int i = 0; i < 1; i++) {
         	cartFrozenFood1.addItem(new FrozenFood());
         }
         
         cartFrozenFood1Expected = 5.4; 
         
-        cartFrozenFood5 = createCart(30);
+        cartFrozenFood5 = createCart(21);
         for (int i = 0; i < 5; i++) {
         	cartFrozenFood5.addItem(new FrozenFood());
         }
         
         cartFrozenFood5Expected = 27; 
         
-        cartFrozenFood10 = createCart(30);
+        cartFrozenFood10 = createCart(21);
         for (int i = 0; i < 10; i++) {
         	cartFrozenFood10.addItem(new FrozenFood());
         }
         
         cartFrozenFood10Expected = 54; 
         
-        cartFrozenFood100 = createCart(30);
+        cartFrozenFood100 = createCart(21);
         for (int i = 0; i < 100; i++) {
         	cartFrozenFood100.addItem(new FrozenFood());
         }
@@ -285,7 +343,7 @@ public class BlackBoxGiven {
         cartFrozenFood100Expected = 540; 
         
         
-        cartAlcFrozen2 = createCart(30);
+        cartAlcFrozen2 = createCart(21);
         for (int i =0; i < 2; i++) {
         	cartAlcFrozen2.addItem(new Alcohol());
         	cartAlcFrozen2.addItem(new FrozenFood());; 
@@ -293,7 +351,7 @@ public class BlackBoxGiven {
         
        cartAlcFrozenExpected2 = 21.6; 
        
-       mixedCartNoDisc = createCart(30);
+       mixedCartNoDisc = createCart(21);
        for (int i =0; i< 1; i++) {
     	   mixedCartNoDisc.addItem(new Meat());
     	   mixedCartNoDisc.addItem(new Dairy());
@@ -301,7 +359,7 @@ public class BlackBoxGiven {
        }
        mixedCartNoDiscExpected = 19.44;
        
-       mixedCartDiscType1 = createCart(30);
+       mixedCartDiscType1 = createCart(21);
        for (int i = 0; i < 1; i++) {
     	   mixedCartDiscType1.addItem(new Dairy());
     	   mixedCartDiscType1.addItem(new Meat());
@@ -312,8 +370,9 @@ public class BlackBoxGiven {
        }
        
        mixedCartDiscType1Expected = 19.44; 
+       amountSavedCartDiscType1Expected = 1.08;
        
-       mixedCartDiscType2 = createCart(30);
+       mixedCartDiscType2 = createCart(21);
        for (int i = 0; i< 1; i++) {
     	   mixedCartDiscType2.addItem(new Alcohol());
     	   mixedCartDiscType2.addItem(new FrozenFood());
@@ -323,8 +382,9 @@ public class BlackBoxGiven {
        }
        
        mixedCartDiscType2Expected = 24.84; 
+       amountSavedCartDiscType2Expected = 3.24;
        
-       mixedCartDiscTwoTypes = createCart(30);
+       mixedCartDiscTwoTypes = createCart(21);
        for (int i = 0; i<1; i++) {
     	   mixedCartDiscTwoTypes.addItem(new Dairy());
     	   mixedCartDiscTwoTypes.addItem(new Produce());
@@ -336,6 +396,212 @@ public class BlackBoxGiven {
 
        }
        mixedCartTwoTypesExpected = 30.24; 
+       amountSavedCartDiscTwoTypesExpected = 4.32; 
+       
+       
+       
+       
+       
+       ////////////////////////////////////////
+       
+       cartAlcohol1UnderAge = createCart(15);
+       for (int i = 0; i < 1; i++) {
+       	cartAlcohol1UnderAge.addItem(new Alcohol());
+       }
+       
+       
+       
+       cartAlcohol5UnderAge = createCart(15);
+       for (int i = 0; i < 5; i++) {
+       	cartAlcohol5UnderAge.addItem(new Alcohol());
+       }
+       
+       
+       
+       cartAlcohol10UnderAge = createCart(15);
+       for (int i = 0; i < 10; i++) {
+       	cartAlcohol10UnderAge.addItem(new Alcohol());
+       }
+       
+       
+       
+       cartAlcohol100UnderAge = createCart(15);
+       for (int i = 0; i < 100; i++) {
+       	cartAlcohol100UnderAge.addItem(new Alcohol());
+       }
+       
+       
+       /////////////////////////////////////////////
+       
+       cartAlcohol121 = createCart(30);
+       for (int i = 0; i < 1; i++) {
+       	cartAlcohol121.addItem(new Alcohol());
+       }
+       
+       cartAlcohol1Expected21 = 8.64;
+       
+       cartAlcohol521 = createCart(30);
+       for (int i = 0; i < 5; i++) {
+       	cartAlcohol521.addItem(new Alcohol());
+       }
+       
+       cartAlcohol5Expected21 = 43.2;
+       
+       cartAlcohol1021 = createCart(30);
+       for (int i = 0; i < 10; i++) {
+       	cartAlcohol1021.addItem(new Alcohol());
+       }
+       
+       cartAlcohol10Expected21 = 86.4;
+       
+       cartAlcohol10021 = createCart(30);
+       for (int i = 0; i < 100; i++) {
+       	cartAlcohol10021.addItem(new Alcohol());
+       }
+       
+       cartAlcohol100Expected21 = 864;
+       
+       cartEmpty = createCart(30);
+      
+       
+       cartEmptyExpected = 0.0;
+       
+       cartEmpty21 = createCart(21);
+     
+       
+       cartEmpty21Expected = 0.0; 
+       
+       cartEmptyUnderAge = createCart(15);
+      
+       cartEmptyUnderAgeExpected = 0.0; 
+       
+       //edgeCases
+       
+       edgeCaseAge20 = createCart(20);
+       for (int i = 0; i < 2; i++) {
+           edgeCaseAge20.addItem(new Alcohol());
+       }
+       for(int i = 0; i < 3; i++) {
+           edgeCaseAge20.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseAge20.addItem(new Meat());
+       }
+       
+       edgeCaseAge22 = createCart(22);
+       for (int i = 0; i < 2; i++) {
+           edgeCaseAge22.addItem(new Alcohol());
+       }
+       for(int i = 0; i < 3; i++) {
+           edgeCaseAge22.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseAge22.addItem(new Meat());
+       }
+       edgeCaseAge22Expected = 70.2; 
+       
+       //edgeCasesProduce
+       
+       edgeCaseProduce1 = createCart(20);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseProduce1.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseProduce1.addItem(new Meat());
+       }
+       for(int i =0; i< 2; i++) {
+    	   edgeCaseProduce1.addItem(new Produce());
+    	  
+       }
+       
+       edgeCaseProduce1Expected = 57.24;
+       
+       edgeCaseProduce2 = createCart(20);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseProduce2.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseProduce2.addItem(new Meat());
+       }
+       for(int i =0; i< 4; i++) {
+    	   edgeCaseProduce2.addItem(new Produce());
+    	  
+       }
+       
+       edgeCaseProduce2Expected = 60.48; 
+       
+       /////////////////////////////
+       
+       edgeCaseAlcFroz1 = createCart(22);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseAlcFroz1.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseAlcFroz1.addItem(new Meat());
+       }
+       for(int i = 0; i < 2; i++) {
+    	   edgeCaseAlcFroz1.addItem(new Alcohol());;
+    	   
+       }
+       edgeCaseAlcFroz1.addItem(new FrozenFood());
+       
+       edgeCaseAlcFroz1Expected = 72.36; 
+       
+       //////////////////////////////////////
+       
+       edgeCaseNoAlc1 = createCart(20);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseNoAlc1.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseNoAlc1.addItem(new Meat());
+       }
+       
+       edgeCaseNoAlc1Expected = 52.92; 
+       
+       
+       edgeCaseNoAlc2 = createCart(21);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseNoAlc2.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseNoAlc2.addItem(new Meat());
+       }
+       
+       edgeCaseNoAlc2Expected = 52.92;
+       
+       
+       edgeCaseNoAlc3 = createCart(22);
+       for(int i = 0; i < 3; i++) {
+           edgeCaseNoAlc3.addItem(new Dairy());
+       }
+       for(int i = 0; i < 4; i++) {
+           edgeCaseNoAlc3.addItem(new Meat());
+       }
+       
+       edgeCaseNoAlc3Expected = 52.92;
+       
+       
+       
+       
+       
+       
+   
+       
+       
+       
+       
+       
+       
+       
+       
+     
+       
+       
+       
+      
+       
+       
        
        
        
@@ -515,6 +781,132 @@ public class BlackBoxGiven {
     	double amount = mixedCartDiscTwoTypes.calcCost();
     	assertEquals(mixedCartTwoTypesExpected, amount, .01); 
     }
+    
+    ////////////////////////////////////////////////////////
+    
+    @Test(expected = UnderAgeException.class)
+    public void underAgeAlcohol1() throws UnderAgeException{
+    	 double buffer = cartAlcohol1UnderAge.calcCost(); 
+    }
+    
+    @Test(expected = UnderAgeException.class)
+    public void underAgeAlcohol5() throws UnderAgeException{
+    	 double buffer = cartAlcohol5UnderAge.calcCost(); 
+    }
+    
+    @Test(expected = UnderAgeException.class)
+    public void underAgeAlcohol10() throws UnderAgeException{
+    	 double buffer = cartAlcohol10UnderAge.calcCost(); 
+    }
+    
+    @Test(expected = UnderAgeException.class)
+    public void underAgeAlcohol100() throws UnderAgeException{
+    	 double buffer = cartAlcohol100UnderAge.calcCost(); 
+    }
+    
+    ////////////////////////////////////////////////////////
+    
+    @Test
+    public void calcCostCartAlcohol121() throws UnderAgeException {
+    	double amount = cartAlcohol121.calcCost();
+    	assertEquals(cartAlcohol1Expected21, amount, .01);
+    	
+    }
+    
+    @Test
+    public void calcCostCartAlcohol521() throws UnderAgeException {
+    	double amount = cartAlcohol521.calcCost();
+    	assertEquals(cartAlcohol5Expected21, amount, .01);
+    	
+    }
+    
+    @Test
+    public void calcCostCartAlcohol1021() throws UnderAgeException {
+    	double amount = cartAlcohol1021.calcCost();
+    	assertEquals(cartAlcohol10Expected21, amount, .01);
+    	
+    }
+    
+    @Test
+    public void calcCostCartAlcohol10021() throws UnderAgeException {
+    	double amount = cartAlcohol10021.calcCost();
+    	assertEquals(cartAlcohol100Expected21, amount, .01);
+    	
+    }
+    
+    @Test
+    public void calcCostCartEmpty() throws UnderAgeException {
+    	double amount = cartEmpty.calcCost();
+    	assertEquals(cartEmptyExpected, amount, .01);
+    }
+    
+    @Test
+    public void calcCostCartEmpty21() throws UnderAgeException {
+    	double amount = cartEmpty21.calcCost();
+    	assertEquals(cartEmpty21Expected, amount, .01);
+    }
+    
+    @Test
+    public void calcCostCartEmptyUnderAge() throws UnderAgeException {
+    	double amount = cartEmptyUnderAge.calcCost();
+    	assertEquals(cartEmptyUnderAgeExpected, amount, .01);
+    }
+    
+    
+    ////////////////////////////////////////////////////////////////
+    
+    @Test(expected = UnderAgeException.class)
+    public void testedgeCaseAge20() throws UnderAgeException{
+    	 double buffer = edgeCaseAge20.calcCost(); 
+    }
+    
+    @Test
+    public void testedgeCaseAge22() throws UnderAgeException {
+    	double amount = edgeCaseAge22.calcCost(); 
+    	assertEquals(edgeCaseAge22Expected, amount, .01); 
+    }
+    
+    @Test
+    public void testedgeCaseProduce1() throws UnderAgeException {
+    	double amount = edgeCaseProduce1.calcCost();
+    	assertEquals(edgeCaseProduce1Expected, amount, .01); 
+    }
+    
+    @Test
+    public void testedgeCaseProduce() throws UnderAgeException {
+    	double amount = edgeCaseProduce2.calcCost();
+    	assertEquals(edgeCaseProduce2Expected, amount, .01);
+    }
+    
+    @Test
+    public void testedgeCaseAlcFroz1() throws UnderAgeException {
+    	double amount = edgeCaseAlcFroz1.calcCost();
+    	assertEquals(edgeCaseAlcFroz1Expected, amount, .01); 
+    }
+    
+    @Test
+    public void testegdeCaseNoAlc1() throws UnderAgeException {
+    	double amount = edgeCaseNoAlc1.calcCost();
+    	assertEquals(edgeCaseNoAlc1Expected, amount, 0.01); 
+    }
+    
+    @Test
+    public void testegdeCaseNoAlc2() throws UnderAgeException {
+    	double amount = edgeCaseNoAlc2.calcCost();
+    	assertEquals(edgeCaseNoAlc2Expected, amount, 0.01); 
+    }
+    
+    @Test
+    public void testegdeCaseNoAlc3() throws UnderAgeException {
+    	double amount = edgeCaseNoAlc3.calcCost();
+    	assertEquals(edgeCaseNoAlc3Expected, amount, 0.01); 
+    }
+  
+  
+    
+    
+    
+    
     
     
     
